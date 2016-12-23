9be5f79cb568a8e4c49509b313eb71c0d73cef0c
waqasjamal <waqasjamal.wfh@gmail.com>
2016-12-23 18:50:45 +0500

    procfile

--- config/db.js
+++ config/db.js
@@ -30,7 +30,7 @@ var Server = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure;
 
-var server = new Server('mongodb://<dbuser>:<dbpassword>@ds141428.mlab.com:41428/fyp_project', 41428, {auto_reconnect: true});
+var server = new Server('mongodb://ds141428.mlab.com:41428/fyp_project', 41428, {auto_reconnect: true});
 db = new Db('fyp_project', server);
 
 db.open(function(err, db) {
