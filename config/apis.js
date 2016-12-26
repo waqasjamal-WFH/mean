d62d16831bcd8ca7ae41679974f4b3ba3e1107f0
waqasjamal <waqasjamal.wfh@gmail.com>
2016-12-26 11:29:37 +0500

    server config reg patient

--- config/apis.js
+++ config/apis.js
@@ -1,85 +1,63 @@
 exports.register = function(req, res) {
 	var data = req.body;
+
+    var MongoClient = require('mongodb').MongoClient
+      , Server = require('mongodb').Server;
+
+    var mongoClient = new MongoClient(new Server('ds141428.mlab.com', 41428));
+    mongoClient.open(function(err, mongoClient) {
+
+        if(!err) {
+            
+            var db = mongoClient.db("fyp_project");
+            db.authenticate('hello', 'hello', function(err, result) {
+                db.collection('register', function(err, collection) {   
+                    collection.insert(data, {safe:true}, function(err, result) {
+                        if (err) {
+                            res.send({err});
+                        } else {
+                            console.log('Success: ' + '1');
+                            res.send('1');
+                        }
+                    });
+                });  
+            });
+        }
+    });
     // res.send('Adding wine: ' + data.username);
     // res.end();
-    db.collection('register', function(err, collection) {
-        collection.insert(data, {safe:true}, function(err, result) {
-            if (err) {
-                res.send({err});
-            } else {
-                console.log('Success: ' + '1');
-                res.send('1');
-            }
-        });
-    });
+    
 };
 
 exports.register_parient = function(req, res ) {
     var data = req.body;
     // res.send('Adding wine: ' + data.username);
     // res.end();
+    var MongoClient = require('mongodb').MongoClient
+      , Server = require('mongodb').Server;
 
+    var mongoClient = new MongoClient(new Server('ds141428.mlab.com', 41428));
+    mongoClient.open(function(err, mongoClient) {
 
-
-var MongoClient = require('mongodb').MongoClient
-  , Server = require('mongodb').Server;
-
-var mongoClient = new MongoClient(new Server('ds141428.mlab.com', 41428));
-mongoClient.open(function(err, mongoClient) {
-
-    if(!err) {
-        
-//         var db = mongoclient.db("fyp_project");
-  var db = mongoClient.db("fyp_project");
-
-
-
-db.authenticate('hello', 'hello', function(err, result) {
-    // On authorized result=true
-    // Not authorized result=false
-    db.collection('register_patient', function(err, collection) {
-        collection.insert(data, {safe:true}, function(err, result) {
-            if (err) {
-                res.send(err);
-            } else {
-                console.log('Success: ' + '1');
-                res.send('1');
-            }
-        });
+        if(!err) {
+            
+            var db = mongoClient.db("fyp_project");
+            db.authenticate('hello', 'hello', function(err, result) {
+                // On authorized result=true
+                // Not authorized result=false
+                db.collection('register_patient', function(err, collection) {
+                    collection.insert(data, {safe:true}, function(err, result) {
+                        if (err) {
+                            res.send(err);
+                        } else {
+                            console.log('Success: ' + '1');
+                            res.send('1');
+                        }
+                    });
+                });
+            });
+        }
     });
-
-
-    // mongoClient.close();
-// console.log(db.collection);
-    // If authorized you can use the database in the db variable
-  });
-
-
-
-
-
-
-
-  // console.log(db);
-}
-// exports.db =db;
-});
-
-
-
-
-
-
-    // db.collection('register_patient', function(err, collection) {
-    //     collection.insert(data, {safe:true}, function(err, result) {
-    //         if (err) {
-    //             res.send({err});
-    //         } else {
-    //             console.log('Success: ' + '1');
-    //             res.send('1');
-    //         }
-    //     });
-    // });
 };
 
 exports.login = function(req, res) {
