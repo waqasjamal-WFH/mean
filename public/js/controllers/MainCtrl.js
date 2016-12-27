fcb7ca2b135550ade14ae3cd674e482aa6046659
waqasjamal <waqasjamal.wfh@gmail.com>
2016-12-27 16:16:38 +0500

    final commit

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,4 +1,4 @@
-angular.module('MainCtrl', ['ui.calendar']).controller('MainController', function($scope,$location,$localStorage,uiCalendarConfig) {
+angular.module('MainCtrl', ['ui.calendar']).controller('MainController', function($scope,$location,$localStorage,$compile,uiCalendarConfig) {
 
 	
 
