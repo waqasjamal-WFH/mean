86c55e216567b6cd61e941ee69127a98eb2df0cc
waqasjamal <waqasjamal.wfh@gmail.com>
2016-12-27 14:27:25 +0500

    final commit

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,7 +1,7 @@
 angular.module('MainCtrl', ['ui.calendar']).controller('MainController', function($scope,$location,$localStorage,$compile,uiCalendarConfig) {
 
 	$scope.events = [
-      {title: 'All Day Event'}
+      {title: 'All Day Event',start : 'Tue Dec 27 2016 14:26:34 GMT+0500'}
       
     ];
 	$scope.logout= function(){
