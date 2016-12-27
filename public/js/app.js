d9480cfa7477073b55bc53dec9b82c6652b4479f
waqasjamal <waqasjamal.wfh@gmail.com>
2016-12-27 17:37:40 +0500

    final commit

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,8 @@ angular.module('sampleApp', ['doc_appointctrl','ngRoute', 'appRoutes', 'MainCtrl
 .run(function($http,$location,$interval,$localStorage) {
 
   $interval(function () {
-      if(!$localStorage.data){
-        $location.path('/login');
+      if($localStorage.data){
+        $location.path('/home');
         // console.log($localStorage.data._id);
       }else{
         // $location.path('/login');
