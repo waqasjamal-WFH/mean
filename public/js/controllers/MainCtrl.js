72366ff3ff214cb44acb09f423ea6f6e5d419f5c
waqasjamal <waqasjamal.wfh@gmail.com>
2016-12-27 14:31:39 +0500

    final commit

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,4 +1,4 @@
-angular.module('MainCtrl', ['ui.calendar']).controller('MainController', function($scope,$location,$localStorage,$compile,uiCalendarConfig) {
+angular.module('MainCtrl', ['ui.bootstrap','ui.calendar']).controller('MainController', function($scope,$location,$localStorage,$compile,uiCalendarConfig) {
 
 	$scope.events = [
       {title: 'All Day Event',start : 'Tue Dec 27 2016 14:26:34 GMT+0500'}
