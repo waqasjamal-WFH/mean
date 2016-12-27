5ef7cdeef9c560799f3641dab14aa41817f6f723
waqasjamal <waqasjamal.wfh@gmail.com>
2016-12-27 16:52:04 +0500

    final commit

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -124,8 +124,8 @@ angular.module('MainCtrl', ['ui.calendar']).controller('MainController', functio
       }
     };
     /* event sources array*/
-    $scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
-    $scope.eventSources2 = [$scope.calEventsExt, $scope.eventsF, $scope.events];
+    $scope.eventSources = [$scope.events];
+    // $scope.eventSources2 = [$scope.calEventsExt, $scope.eventsF, $scope.events];
 
 
 
