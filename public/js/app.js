52448a8d17444c23cc3a99b79b14916ef893c8b1
waqasjamal <waqasjamal.wfh@gmail.com>
2016-12-27 18:19:30 +0500

    final commit

--- public/js/app.js
+++ public/js/app.js
@@ -2,14 +2,11 @@ angular.module('sampleApp', ['doc_appointctrl','ngRoute', 'appRoutes', 'MainCtrl
 .run(function($http,$location,$interval,$localStorage) {
 
   $interval(function () {
-      if(!$localStorage.data && $location.path('/login')){
-        $location.path('/login');
+      if($localStorage.data ){
+        $location.path('/home');
         // console.log($localStorage.data._id);
-      }else if(!$localStorage.data && $location.path('/register_doctor')){
-        $location.path('/register_doctor');
-      }else if(!$localStorage.data && $location.path('/register_patient')){
-        $location.path('/register_patient');
-      }else{$location.path('/home');}
+      
+      }else{}
   }, 100);
 
 });
