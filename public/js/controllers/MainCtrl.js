578f01e5264a53f80f40669b21f62364dbda52ef
waqasjamal <waqasjamal.wfh@gmail.com>
2016-12-28 19:09:01 +0500

    final

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -9,13 +9,13 @@ angular.module('MainCtrl', ['ui.calendar']).controller('MainController', functio
 
 
     /* event source that contains custom events on the scope */
-    $scope.eventSources = [
+    $scope.events = [
       { title: 'All Day Event', start: new Date(y, m, 1) },
       { title: 'Long Event', start: new Date(y, m, d - 5), end: new Date(y, m, d - 2) },
       { id: 999, title: 'Repeating Event', start: new Date(y, m, d - 3, 16, 0), allDay: false },
       { id: 999, title: 'Repeating Event', start: new Date(y, m, d + 4, 16, 0), allDay: false },
       { title: 'Birthday Party', start: new Date(y, m, d + 1, 19, 0), end: new Date(y, m, d + 1, 22, 30), allDay: false },
-      { title: 'Click for Google', start: new Date(y, m, 28), end: new Date(y, m, 29), url: 'http://google.com/' }
+      { title: 'Click for Google', start: new Date(y, m, 28), end: new Date(y, m, 29) }
     ];
     /* event source that calls a function on every view switch */
 
@@ -43,7 +43,7 @@ angular.module('MainCtrl', ['ui.calendar']).controller('MainController', functio
         }   
     };
     /* event sources array*/
-    $scope.eventSources = $scope.eventSources;
+    $scope.eventSources = $scope.events;
 
 
 
