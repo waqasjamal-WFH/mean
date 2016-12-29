ae86de92b7eb8be4532621612e2ad0cb5b86e528
waqasjamal <waqasjamal.wfh@gmail.com>
2016-12-29 18:57:43 +0500

    final commit

--- public/js/controllers/doc_appointctrl.js
+++ public/js/controllers/doc_appointctrl.js
@@ -23,7 +23,7 @@ angular.module('doc_appointctrl', []).controller('docapoint_controller', functio
         if(data){
 
          $.each(data, function(key, value) {   
-            $('#option').append('<option value="doctor">'+value+'</option>');
+            $('#option').append('<option value="doctor">'+value.firstname+'</option>');
                     
 });
 
