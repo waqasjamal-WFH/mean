2a9787d9820bd6b331c40b8d86b890e116f54d13
waqasjamal <waqasjamal.wfh@gmail.com>
2016-12-29 17:20:33 +0500

    final commit

--- public/js/controllers/doc_appointctrl.js
+++ public/js/controllers/doc_appointctrl.js
@@ -2,23 +2,20 @@ angular.module('doc_appointctrl', []).controller('docapoint_controller', functio
 
 	$scope.register = function(){
 		 var data = {
-          firstname: $scope.firstname,
-          address: $scope.address,
-          email: $scope.email,
-          degree: $scope.degree,
-          job: $scope.job,
-          number: $scope.number,
-          lastname: $scope.lastname,
-          pass : $scope.pass
+          firstname: $scope.disease,
+          address: $scope.date,
+          datee: $('#option').val()
       };
 
-      $http.post("/regis", data).success(function(data, status) {
-        if(status= '1'){
-          console.log("success")
-        }else{
-          console.log("fail")
-        }
-      })
+      console.log(data);
+
+      // $http.post("/regis", data).success(function(data, status) {
+      //   if(status= '1'){
+      //     console.log("success")
+      //   }else{
+      //     console.log("fail")
+      //   }
+      // })
 
 	};	
 
