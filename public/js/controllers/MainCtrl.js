e970c40157d42ce5957331dead5424e6c6b2c444
waqasjamal <waqasjamal.wfh@gmail.com>
2016-12-29 16:01:04 +0500

    final commit

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -15,69 +15,69 @@ var date = new Date();
   ];
   
   // Clear events via splice(0) and then push into events source.
-  $scope.getEventsEmptySplice = function () {
-    console.log('Clearing $scope.events via splice(0)');
+  // $scope.getEventsEmptySplice = function () {
+  //   console.log('Clearing $scope.events via splice(0)');
     
-    // Clearing in this manner maintains the two-way data bind.
-    // This can be called over and over, with old events cleared,
-    // and new random events displayed. This no longer works
-    // if getEventsEmptyArray is ever called, due to two-way
-    // data bind being broken within that function.
-    $scope.events.splice(0);
+  //   // Clearing in this manner maintains the two-way data bind.
+  //   // This can be called over and over, with old events cleared,
+  //   // and new random events displayed. This no longer works
+  //   // if getEventsEmptyArray is ever called, due to two-way
+  //   // data bind being broken within that function.
+  //   $scope.events.splice(0);
     
-    // Get 3 random days, 1-28
-    var day1 = Math.floor(Math.random() * (28 - 1)) + 1;
-    var day2 = Math.floor(Math.random() * (28 - 1)) + 1;
-    var day3 = Math.floor(Math.random() * (28 - 1)) + 1;
+  //   // Get 3 random days, 1-28
+  //   var day1 = Math.floor(Math.random() * (28 - 1)) + 1;
+  //   var day2 = Math.floor(Math.random() * (28 - 1)) + 1;
+  //   var day3 = Math.floor(Math.random() * (28 - 1)) + 1;
     
-    // Simulating an AJAX request with $timeout.
-    $timeout(function () {
-      // Create temp events array.
-      var newEvents = [
-        {title: 'Random 1', start: new Date(y, m, day1), allDay: true},
-        {title: 'Random 2', start: new Date(y, m, day2), allDay: true},
-        {title: 'Random 3', start: new Date(y, m, day3), allDay: true}
-      ];
-      // Push newEvents into events, one by one.
-      angular.forEach(newEvents, function (event) {
-        $scope.events.push(event);
-      });
-      console.log('New Events pushed');
-    }, 100);
-  };
+  //   // Simulating an AJAX request with $timeout.
+  //   $timeout(function () {
+  //     // Create temp events array.
+  //     var newEvents = [
+  //       {title: 'Random 1', start: new Date(y, m, day1), allDay: true},
+  //       {title: 'Random 2', start: new Date(y, m, day2), allDay: true},
+  //       {title: 'Random 3', start: new Date(y, m, day3), allDay: true}
+  //     ];
+  //     // Push newEvents into events, one by one.
+  //     angular.forEach(newEvents, function (event) {
+  //       $scope.events.push(event);
+  //     });
+  //     console.log('New Events pushed');
+  //   }, 100);
+  // };
 
   // Clear events via empty array and then push into events source.
-  $scope.getEventsEmptyArray = function () {
-    console.log('Clearing $scope.events via assigning empty array');
+  // $scope.getEventsEmptyArray = function () {
+  //   console.log('Clearing $scope.events via assigning empty array');
     
-    // Clearing in this manner, assigns a new empty array
-    // to $scope.events, and breaks the two-way data bind.
-    // Once this is called, no update of $scope.events will
-    // change the calendar, because the events array in the controller
-    // scope is different from the events array in the calendar
-    // directive isolate scope.
-    $scope.events = [];
+  //   // Clearing in this manner, assigns a new empty array
+  //   // to $scope.events, and breaks the two-way data bind.
+  //   // Once this is called, no update of $scope.events will
+  //   // change the calendar, because the events array in the controller
+  //   // scope is different from the events array in the calendar
+  //   // directive isolate scope.
+  //   $scope.events = [];
     
-    // Get 3 random days, 1-28
-    var day1 = Math.floor(Math.random() * (28 - 1)) + 1;
-    var day2 = Math.floor(Math.random() * (28 - 1)) + 1;
-    var day3 = Math.floor(Math.random() * (28 - 1)) + 1;
+  //   // Get 3 random days, 1-28
+  //   var day1 = Math.floor(Math.random() * (28 - 1)) + 1;
+  //   var day2 = Math.floor(Math.random() * (28 - 1)) + 1;
+  //   var day3 = Math.floor(Math.random() * (28 - 1)) + 1;
     
-    // Simulating an AJAX request with $timeout.
-    $timeout(function () {
-      // Create temp events array.
-      var newEvents = [
-        {title: 'Random 1', start: new Date(y, m, day1), allDay: true},
-        {title: 'Random 2', start: new Date(y, m, day2), allDay: true},
-        {title: 'Random 3', start: new Date(y, m, day3), allDay: true}
-      ];
-      // Push newEvents into events, one by one.
-      angular.forEach(newEvents, function (event) {
-        $scope.events.push(event);
-      });
-      console.log('New Events pushed');
-    }, 100);
-  };
+  //   // Simulating an AJAX request with $timeout.
+  //   $timeout(function () {
+  //     // Create temp events array.
+  //     var newEvents = [
+  //       {title: 'Random 1', start: new Date(y, m, day1), allDay: true},
+  //       {title: 'Random 2', start: new Date(y, m, day2), allDay: true},
+  //       {title: 'Random 3', start: new Date(y, m, day3), allDay: true}
+  //     ];
+  //     // Push newEvents into events, one by one.
+  //     angular.forEach(newEvents, function (event) {
+  //       $scope.events.push(event);
+  //     });
+  //     console.log('New Events pushed');
+  //   }, 100);
+  // };
 
   // Assign the 2 sources to $scope.eventSources for calendar.
   $scope.eventSources = [$scope.staticEvents, $scope.events];
