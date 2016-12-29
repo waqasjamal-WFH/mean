6eff0dd573d3de81bfaa7e8a502444fc5a51b834
waqasjamal <waqasjamal.wfh@gmail.com>
2016-12-29 19:52:04 +0500

    final commit

--- public/js/controllers/doc_appointctrl.js
+++ public/js/controllers/doc_appointctrl.js
@@ -1,14 +1,16 @@
-angular.module('doc_appointctrl', []).controller('docapoint_controller', function($scope, $http) {
+angular.module('doc_appointctrl', []).controller('docapoint_controller', function($scope, $http,$localStorage) {
 
 	$scope.register = function(){
 		 var data = {
-          firstname: $scope.disease,
+          patient_id : $localStorage.data._id,
+          disease: $scope.disease,
           datee: $scope.date,
           doc_id: $('#option').val(),
-          doc_name: $('#option option:selected').text()
+          doc_name: $('#option option:selected').text(),
+          statuss : 0
       };
 
-      console.log($('#option'));
+      console.log($localStorage.data._id);
       console.log(data);
 
       // $http.post("/regis", data).success(function(data, status) {
